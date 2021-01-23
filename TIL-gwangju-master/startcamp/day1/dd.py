cd80cb49313246f945193fefcc9b2c36f6cdd5f6
Wonsang Joo <nigagaraha@naver.com>
2021-01-23 22:21:35 +0900

    01-23 공부

---
+++ TIL-gwangju-master/startcamp/day1/dd.py
@@ -0,0 +1,7 @@
+num = int(input())
+
+start = 0
+
+while start < num:
+    print(num)
+    start += 1
--- TIL-gwangju-master/startcamp/day1/hello.py
+++ TIL-gwangju-master/startcamp/day1/hello.py
@@ -1,10 +1,10 @@
 greeting = '안녕하세요!'
 
-# 1. while문 반복
-# count = 0
-# while count < 5:
-#     print(greeting)
-#     count = count + 1
+ 1. while문 반복
+ count = 0
+ while count < 5:
+     print(greeting)
+     count = count + 1
 
 # 2. for문 반복
 # count_list = [0, 1, 2, 3]
@@ -12,5 +12,4 @@ greeting = '안녕하세요!'
 #     print(greeting)
 
 # 3. for문 반복 (함수 활용)
-for num in range(4):
-    print(greeting)
+#
